1b3d5305c88d102420c72f98430819dcab919a0f
Matte <mattias.johansson@ivbar.com>
2020-09-28 11:54:07 +0200

    Added email to fix build warning

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/folkol/yg',
     author='folkol',
+    author_email='mattias4@kth.se',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
